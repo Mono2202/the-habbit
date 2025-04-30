2b50d4269579dd5e14716dc3bfc87dc12b231943
Mono <monocode22@gmail.com>
2025-04-30 19:58:55 +0300

    Fixed existing user joining

--- src/main.py
+++ src/main.py
@@ -22,22 +22,22 @@ async def set_commands(app):
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.effective_user
     try:
-        user_file_path = [entry for entry in os.listdir(USER_DATABASE_PATH) if entry.startswith(str(user.id)) and os.path.isfile(entry)][0]
+        user_file_path = [entry for entry in os.listdir(USER_DATABASE_PATH) if entry.startswith(str(user.id)) and os.path.isfile(f"{USER_DATABASE_PATH}/{entry}")][0]
         await update.message.reply_text(f"Hello {user.name}! Welcome back :)")
     except IndexError:
         user_file_path = f"{USER_DATABASE_PATH}/{user.id}.json"
         await update.message.reply_text(f"Hi {user.name}! Welcome to your Habit Tracker! Let's start :)")
-    
-    with open(user_file_path, "a+") as user_file:
-        json.dump(
-            {
-                "username": user.name,
-                "join_date": datetime.today().strftime("%d/%m/%Y")
-            },
-            user_file,
-            indent=4
-        )
-        
+
+        with open(user_file_path, "a+") as user_file:
+            json.dump(
+                {
+                    "username": user.name,
+                    "join_date": datetime.today().strftime("%d/%m/%Y")
+                },
+                user_file,
+                indent=4
+            )
+
 
 async def send_image(update: Update, context: ContextTypes.DEFAULT_TYPE):
     with open(".\\assets\\golbat.png", "rb") as img:

4345f458191c90aaa760b6aad476bb4f3c1312a4
Mono <monocode22@gmail.com>
2025-05-01 16:13:04 +0300

    Added remove habit command

--- src/commands.py
+++ src/commands.py
@@ -25,13 +25,24 @@ async def add_habit(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def list_habits(update: Update, context: ContextTypes.DEFAULT_TYPE):
     habits_list = ""
-    for habit in context.user.habits:
-        habits_list += rf"*{habit.name}* \[{habit.frequency}, {habit.points} pts]"
+    for i, habit in enumerate(context.user.habits):
+        habits_list += rf"*{i}. {habit.name}* \[{habit.frequency}, {habit.points} pts]"
         habits_list += "\n"
     await update.message.reply_text(habits_list, parse_mode=ParseMode.MARKDOWN)
+
+async def remove_habit(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # TODO: add input check
+    try:
+        habit_index = int(context.args[0])
+        removed_habit = context.user.habits[habit_index]
+        del context.user.habits[habit_index]
+        await update.message.reply_text(f"🗑️ Habit `{removed_habit.name}` removed!", parse_mode=ParseMode.MARKDOWN)
+    except Exception:
+        await update.message.reply_text(f"❌ Invalid command", parse_mode=ParseMode.MARKDOWN)
 
 COMMANDS = [
     Command(start, "Starts the bot"),
     Command(add_habit, "Adds a new habit"),
+    Command(remove_habit, "Removes a habit"),
     Command(list_habits, "List all habits"),
 ]

28f9bc188604e1bf79a9d6f00a7ae8fc1cb18e73
Mono <monocode22@gmail.com>
2025-05-21 14:30:07 +0300

    Updated frontend with buttons

--- backend/app.py
+++ backend/app.py
@@ -9,14 +9,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-HABIT_DIFFICULTIES = {
-    "Easy": 1,
-    "Medium": 3,
-    "Hard": 5,
-    "Extreme": 10
-}
+HABIT_DIFFICULTIES = {"Easy": 1, "Medium": 3, "Hard": 5, "Extreme": 10}
 
 DB_LOCK = FileLock("db.json.lock")
+
 
 def user_context(func):
     @wraps(func)
@@ -28,22 +24,27 @@ def user_context(func):
             with open("db.json", "w") as db_file:
                 json.dump(app.config["user"], db_file, indent=4)
         return result
+
     return wrapper
+
 
 @app.route("/api/xp/get_level")
 @user_context
 def get_level():
     return jsonify({"level": app.config["user"]["level"]})
 
+
 @app.route("/api/xp/get_xp")
 @user_context
 def get_xp():
     return jsonify({"xp": app.config["user"]["xp"]})
 
+
 @app.route("/api/xp/get_xp_goal")
 @user_context
 def get_xp_goal():
     return jsonify({"xp_goal": app.config["user"]["xp_goal"]})
+
 
 @app.route("/api/xp/get_user_info")
 @user_context
@@ -55,6 +56,7 @@ def get_user_info():
     }
     return jsonify(user_info)
 
+
 @app.route("/api/xp/gain_xp", methods=["GET"])
 @user_context
 def gain_xp():
@@ -62,9 +64,10 @@ def gain_xp():
 
     if xp_to_add is None:
         return jsonify({"error": "Missing xp"}), 400
-    
+
     gain_xp_handle(xp_to_add)
     return Response(status=200)
+
 
 @app.route("/api/xp/complete_habit", methods=["GET"])
 @user_context
@@ -73,9 +76,10 @@ def complete_habit():
 
     if habit_difficulty is None:
         return jsonify({"error": "Missing habit difficulty"}), 400
-    
+
     gain_xp_handle(HABIT_DIFFICULTIES[habit_difficulty])
     return Response(status=200)
+
 
 def gain_xp_handle(xp_to_add: int):
     app.config["user"]["xp"] += xp_to_add
@@ -84,6 +88,7 @@ def gain_xp_handle(xp_to_add: int):
     while app.config["user"]["xp"] >= app.config["user"]["xp_goal"]:
         app.config["user"]["level"] += 1
         app.config["user"]["xp"] -= app.config["user"]["xp_goal"]
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=5003)
--- frontend/src/pages/LevelPage.js
+++ frontend/src/pages/LevelPage.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
-import { PICTURES } from '../consts/ImageFiles';
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { PICTURES } from "../consts/ImageFiles";
 import PhotoGallery from "../components/PhotoGallery";
 import LoadingWheel from "../components/LoadingWheel";
-import './LevelPage.css';
+import "./LevelPage.css";
 
 function LevelPage() {
   const [user_info, setUserInfo] = useState({
@@ -11,95 +11,122 @@ function LevelPage() {
     xp_goal: 50,
     level: 0,
   });
+  const prevLevel = useRef(user_info.level);
+
   const [loading, setLoading] = useState(true);
   const [selectedPhoto, setSelectedPhoto] = useState(null);
+  const [showConfetti, setShowConfetti] = useState(false);
 
   const difficulties = ["Easy", "Medium", "Hard", "Extreme"];
 
+  const getXP = useCallback(async () => {
+    try {
+      const user_info_result = await fetch(
+        process.env.REACT_APP_BACKEND_URL + "/api/xp/get_user_info"
+      );
+      const user_info_result_json = await user_info_result.json();
+      setUserInfo({
+        xp: user_info_result_json["xp"],
+        xp_goal: user_info_result_json["xp_goal"],
+        level: user_info_result_json["level"],
+        percentage:
+          (user_info_result_json["xp"] / user_info_result_json["xp_goal"]) *
+          100,
+      });
+    } catch (err) {
+      console.error("Error:", err);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
   useEffect(() => {
     getXP();
+  }, [getXP]);
 
-    const interval = setInterval(getXP, 5000);
-    return () => clearInterval(interval);
-  }, []);
-  
   useEffect(() => {
-    if (selectedPhoto == null) {
-      const filteredPics = PICTURES.filter(pic => {
-        const levelNumber = parseInt(pic.split('_')[0].split('/').at(-1));
-        return levelNumber === (parseInt(user_info.level));
-      });
-      setSelectedPhoto(filteredPics[0]);
-    }
-  }, [selectedPhoto, user_info.level])
+    const filteredPics = PICTURES.filter((pic) => {
+      const levelNumber = parseInt(pic.split("_")[0].split("/").at(-1));
+      return levelNumber === parseInt(user_info.level);
+    });
+    setSelectedPhoto(filteredPics[0]);
 
-  async function getXP() {
-    try {
-        const user_info_result = await fetch(process.env.REACT_APP_BACKEND_URL + "/api/xp/get_user_info");
-        const user_info_result_json = await user_info_result.json();
-        setUserInfo({
-          xp: user_info_result_json["xp"],
-          xp_goal: user_info_result_json["xp_goal"],
-          level: user_info_result_json["level"],
-          percentage: (user_info_result_json["xp"] / user_info_result_json["xp_goal"]) * 100,
-        });
-    } catch (err) {
-        console.error("Error:", err);
-    } finally {
-        setLoading(false);
+    if (prevLevel.current === 0) {
+      prevLevel.current = user_info.level;
     }
-  }
+
+    if (user_info.level > prevLevel.current) {
+      setShowConfetti(true);
+      setTimeout(() => setShowConfetti(false), 2000);
+    }
+
+    prevLevel.current = user_info.level;
+  }, [user_info.level]);
 
   const handlePhotoClick = (photoPath) => {
     setSelectedPhoto(photoPath);
   };
 
   const sendHabit = (difficulty) => {
-    fetch(process.env.REACT_APP_BACKEND_URL + `/api/xp/complete_habit?difficulty=${difficulty}`)
-    .then((res) => {
-      if (!res.ok) throw new Error("API error");
-      getXP();
-    })
-    .catch((err) => {
-      console.error("Failed to complete habit:", err);
-    });
+    fetch(
+      process.env.REACT_APP_BACKEND_URL +
+        `/api/xp/complete_habit?difficulty=${difficulty}`
+    )
+      .then((res) => {
+        if (!res.ok) throw new Error("API error");
+        getXP();
+      })
+      .catch((err) => {
+        console.error("Failed to complete habit:", err);
+      });
   };
 
-
   if (loading) {
-    return <LoadingWheel />
-  }
-
-  else {
+    return <LoadingWheel />;
+  } else {
     return (
-        <>
+      <>
         <div className="level-page">
-        <img
-            src={selectedPhoto}
-            alt="Profile"
-            className="profile-pic"
-        />
-        <h2>Level {user_info.level}</h2>
-        <div className="progress-container">
-            <div className="progress-bar" style={{ width: `${user_info.percentage}%` }}></div>
+          <img src={selectedPhoto} alt="Profile" className="profile-pic" />
+          <h2>Level {user_info.level}</h2>
+          <div className="progress-container">
+            <div
+              className="progress-bar"
+              style={{ width: `${user_info.percentage}%` }}
+            ></div>
+          </div>
+          <p>
+            {user_info.xp} / {user_info.xp_goal} XP
+          </p>
         </div>
-        <p>{user_info.xp} / {user_info.xp_goal} XP</p>
-        </div>
+
+        {showConfetti && (
+          <div className="confetti-wrapper">
+            {[...Array(15)].map((_, i) => (
+              <div key={i} className="confetti" style={{ "--i": i / 15 }} />
+            ))}
+          </div>
+        )}
 
         <div className="dark-buttons">
           {difficulties.map((difficulty) => (
-          <button
-            key={difficulty}
-            onClick={() => sendHabit(difficulty)}
-            className={`dark-button ${difficulty.toLowerCase()}`}
-          >
-          {difficulty}
-          </button>
+            <button
+              key={difficulty}
+              onClick={() => sendHabit(difficulty)}
+              className={`dark-button ${difficulty.toLowerCase()}`}
+            >
+              {difficulty}
+            </button>
           ))}
         </div>
 
-        <div><PhotoGallery level={user_info.level} onPhotoClick={handlePhotoClick}/></div>
-        </>
+        <div>
+          <PhotoGallery
+            level={user_info.level}
+            onPhotoClick={handlePhotoClick}
+          />
+        </div>
+      </>
     );
   }
 }
